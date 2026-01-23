48694218f380ab9f1dde945d4e25132482aee0a6
seop <useop0821@gmail.com>
2026-01-24 00:31:06 +0900

    test: 주문 상품 생성 시 재고를 고려한 테스트 추가

--- src/test/java/hello/delivery/order/domain/OrderProductTest.java
+++ src/test/java/hello/delivery/order/domain/OrderProductTest.java
@@ -3,6 +3,7 @@ package hello.delivery.order.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import hello.delivery.product.domain.Product;
+import hello.delivery.product.domain.Stock;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +18,25 @@ class OrderProductTest {
                 .store(null)
                 .name("아메리카노")
                 .price(5000)
+                .build();
+
+        // when
+        OrderProduct orderProduct = OrderProduct.create(product, 2);
+
+        // then
+        assertThat(orderProduct.getProduct().getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    @DisplayName("재고가 있는 주문 상품을 생성할 수 있다.")
+    void createWithStock() throws Exception {
+        // given
+        Product product = Product.builder()
+                .id(1L)
+                .store(null)
+                .name("아메리카노")
+                .price(5000)
+                .stock(Stock.of(10))
                 .build();
 
         // when

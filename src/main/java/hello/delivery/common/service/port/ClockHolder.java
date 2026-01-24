9723550f24cf1fa6dd4172a165e8231170167faa
seop <useop0821@gmail.com>
2026-01-25 01:10:54 +0900

    feat: LocalTime 추가

--- src/main/java/hello/delivery/common/service/port/ClockHolder.java
+++ src/main/java/hello/delivery/common/service/port/ClockHolder.java
@@ -2,10 +2,13 @@ package hello.delivery.common.service.port;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public interface ClockHolder {
 
     LocalDate now();
 
     LocalDateTime nowDateTime();
+
+    LocalTime nowTime();
 }

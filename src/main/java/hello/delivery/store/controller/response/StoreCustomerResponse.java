26d179de36b1473c2fd6e088f40606980d6eeaa0
seop <useop0821@gmail.com>
2026-01-25 01:13:09 +0900

    feat: StoreCustomerResponse에 매장 오픈 상태 및 시간 필드 추가

--- src/main/java/hello/delivery/store/controller/response/StoreCustomerResponse.java
+++ src/main/java/hello/delivery/store/controller/response/StoreCustomerResponse.java
@@ -1,7 +1,9 @@
 package hello.delivery.store.controller.response;
 
+import hello.delivery.common.service.port.ClockHolder;
 import hello.delivery.store.domain.Store;
 import hello.delivery.store.domain.StoreType;
+import java.time.LocalTime;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,27 +15,37 @@ public class StoreCustomerResponse {
     private final String name;
     private final StoreType storeType;
     private final String address;
+    private final boolean isOpen;
+    private final LocalTime openTime;
+    private final LocalTime closeTime;
 
     @Builder
-    private StoreCustomerResponse(Long id, String name, StoreType storeType, String address) {
+    private StoreCustomerResponse(Long id, String name, StoreType storeType, String address, boolean isOpen,
+                                  LocalTime openTime, LocalTime closeTime) {
         this.id = id;
         this.name = name;
         this.storeType = storeType;
         this.address = address;
+        this.isOpen = isOpen;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
     }
 
-    public static StoreCustomerResponse of(Store store) {
+    public static StoreCustomerResponse of(Store store, ClockHolder clockHolder) {
         return StoreCustomerResponse.builder()
                 .id(store.getId())
                 .name(store.getName())
                 .storeType(store.getStoreType())
                 .address(store.getOwner().getAddress())
+                .isOpen(store.isOpening(clockHolder.nowTime()))
+                .openTime(store.getOpenTime())
+                .closeTime(store.getCloseTime())
                 .build();
     }
 
-    public static List<StoreCustomerResponse> of(List<Store> stores) {
+    public static List<StoreCustomerResponse> of(List<Store> stores, ClockHolder clockHolder) {
         return stores.stream()
-                .map(StoreCustomerResponse::of)
+                .map(store -> StoreCustomerResponse.of(store, clockHolder))
                 .toList();
     }
 

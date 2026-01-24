56bb283f9c02e37086f204b2d3fc757e99f03345
seop <useop0821@gmail.com>
2026-01-25 01:16:06 +0900

    test: 오픈,마감 시간을 변경하는 메서드 추가

--- src/test/java/hello/delivery/store/service/StoreServiceImplTest.java
+++ src/test/java/hello/delivery/store/service/StoreServiceImplTest.java
@@ -16,12 +16,16 @@ import hello.delivery.store.domain.Store;
 import hello.delivery.store.domain.StoreCreate;
 import hello.delivery.store.domain.StoreType;
 import hello.delivery.user.domain.User;
+import java.time.LocalTime;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StoreServiceImplTest {
+
+    public static final LocalTime OPEN_TIME = LocalTime.of(12, 0);
+    public static final LocalTime CLOSE_TIME = LocalTime.of(21, 0);
 
     private StoreService storeService;
     private FakeFinder fakeFinder;
@@ -51,6 +55,42 @@ class StoreServiceImplTest {
         assertThat(store.getName()).isEqualTo("한식당");
         assertThat(store.getStoreType()).isEqualTo(KOREAN_FOOD);
         assertThat(store.getTotalSales()).isZero();
+    }
+
+    @Test
+    @DisplayName("오픈시간을 변경 할 수 있다.")
+    void changeOpenTime() throws Exception {
+        //given
+        StoreCreate storeCreate = createStoreCreate("한식당", KOREAN_FOOD);
+        Store store = storeService.create(owner.getId(), storeCreate);
+
+        fakeFinder.addStore(store);
+        LocalTime newOpenTime = LocalTime.of(8, 0);
+
+        //when
+        Store openedStore = storeService.changeOpenTime(store.getId(), newOpenTime);
+
+        //then
+        assertThat(openedStore.getOpenTime()).isNotEqualTo(OPEN_TIME);
+        assertThat(openedStore.isOpening(LocalTime.of(8, 30))).isTrue();
+    }
+
+    @Test
+    @DisplayName("마감시간을 변경 할 수 있다.")
+    void changeCloseTIme() throws Exception {
+        //given
+        StoreCreate storeCreate = createStoreCreate("한식당", KOREAN_FOOD);
+        Store store = storeService.create(owner.getId(), storeCreate);
+
+        fakeFinder.addStore(store);
+        LocalTime newCloseTime = LocalTime.of(23, 0);
+
+        //when
+        Store openedStore = storeService.changeCloseTime(store.getId(), newCloseTime);
+
+        //then
+        assertThat(openedStore.getCloseTime()).isNotEqualTo(CLOSE_TIME);
+        assertThat(openedStore.isOpening(LocalTime.of(22, 50))).isTrue();
     }
 
     @Test
@@ -88,6 +128,8 @@ class StoreServiceImplTest {
         return StoreCreate.builder()
                 .storeName(storeName)
                 .storeType(storeType)
+                .openTime(OPEN_TIME)
+                .closeTime(CLOSE_TIME)
                 .build();
     }
 

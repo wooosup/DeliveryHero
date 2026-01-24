c9596d6c0aa903e126b18ccccf8df4264771533b
seop <useop0821@gmail.com>
2026-01-24 22:46:53 +0900

    refactor: 안쓰는 쿼리 삭제

--- src/main/java/hello/delivery/user/infrastructure/UserJpaRepository.java
+++ src/main/java/hello/delivery/user/infrastructure/UserJpaRepository.java
@@ -2,17 +2,10 @@ package hello.delivery.user.infrastructure;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByUsername(String username);
-
-    @Query("select o from UserEntity o where o.role = 'OWNER' and o.username = :username")
-    Optional<UserEntity> findByOwnerUsername(String username);
-
-    @Query("select c from UserEntity c where c.role = 'CUSTOMER' and c.id = :username")
-    Optional<UserEntity> findByCustomerUsername(String username);
 
     boolean existsByUsername(String username);
 }
